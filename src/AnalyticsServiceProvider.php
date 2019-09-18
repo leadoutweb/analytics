8b20718d47f2d5650ce7d383bef681fe4806b68f
Henrik Mikkelsen <henrik@leadout.dk>
2019-09-18 14:31:36 +0200

    Debug service provider

--- src/AnalyticsServiceProvider.php
+++ src/AnalyticsServiceProvider.php
@@ -2,18 +2,10 @@
 
 namespace Leadout\Analytics;
 
-use App\NewReporting\Analytics\TableFactory;
 use Illuminate\Support\ServiceProvider;
 
 class AnalyticsServiceProvider extends ServiceProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
     /**
      * Register any application services.
      *
@@ -22,15 +14,5 @@ class AnalyticsServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(TableFactory::class);
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return [TableFactory::class];
     }
 }
